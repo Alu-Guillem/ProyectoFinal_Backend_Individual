daeb9a19620b6c0e93d15bff7444d6b0e47ceea8
Mateo Manchola <mmanchola12temporal@gmail.com>
2025-12-12 03:19:03 +0100

    chore: :gear: add Morgan, JS config, and type definitions
    - Add `morgan` for HTTP request logging
    - Introduce `jsconfig.json` to enable JSDoc-based typing in JavaScript
    - Add type definitions for Express, Mongoose, and Node.js to improve editor support and type safety

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from 'express'
-import router from '#i/router.js'
+import router from '#r/router.js'
+import morgan from 'morgan'
 import { HOSTNAME, PORT } from '#c'
-import { connectDB } from './src/libs/database/index.js'
+import { connectDB } from '#libs/database/index.js'
 
 const app = express()
 connectDB()
 
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.use('/api', router)
+
+app.use((req, res) => {
+  res.status(404).json({ message: `Endpoint ${req.originalUrl} not found` })
+})
 
 app.listen(PORT, HOSTNAME, () => {
   console.log(`Servidor en http://${HOSTNAME}:${PORT}`)

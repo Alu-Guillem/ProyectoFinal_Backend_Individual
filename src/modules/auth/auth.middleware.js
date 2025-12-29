a2b8e387fccc43e4a79856cd5512e4d68bfa7cb6
Mateo Manchola <mmanchola12temporal@gmail.com>
2025-12-30 00:51:09 +0100

    feat(auth): :sparkles: add temporal session initialization in authMiddleware

--- src/modules/auth/auth.middleware.js
+++ src/modules/auth/auth.middleware.js
@@ -1,4 +1,7 @@
 export function authMiddleware(req, res, next) {
+  req.session = {}
+  req.session.userId = '507f1f77bcf86cd799439011'
+  req.session.role = 'customer'
   next()
 }
 export function requireRole(role) {

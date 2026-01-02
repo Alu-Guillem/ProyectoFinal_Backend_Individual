24b925d942dd01db373e4e0fd5c66f4c46d9a01f
Mateo Manchola <mmanchola12temporal@gmail.com>
2026-01-02 23:55:36 +0100

    feat(auth): :sparkles: move authentication types to a separate file for better organization

--- types.d.ts
+++ types.d.ts
@@ -1,20 +1,6 @@
-import type { Request } from 'express'
-
 export type commonValidation = (fieldName: string) => fieldValidation
 
 export type fieldValidation = (value: any) => boolean
 
-export interface ValidationSchema {
-  [fieldName: string]: fieldValidation[]
-}
-
-export type Session = {
-  userId: string
-  role: 'admin' | 'employee' | 'customer'
-}
-
-export interface AuthenticatedRequest extends Request {
-  session: Session
-}
-
 export * from '#modules/bookings/bookings.d.ts'
+export * from '#modules/auth/auth.d.ts'

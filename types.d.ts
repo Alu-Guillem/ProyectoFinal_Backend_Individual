07df1f0b5af40e249d80a33b479bd9944b8b9792
Mateo Manchola <mmanchola12temporal@gmail.com>
2026-01-03 02:20:23 +0100

    feat(reviews): :sparkles: add Review interface for managing user reviews

--- types.d.ts
+++ types.d.ts
@@ -3,4 +3,5 @@ export type commonValidation = (fieldName: string) => fieldValidation
 export type fieldValidation = (value: any) => boolean
 
 export * from '#modules/bookings/bookings.d.ts'
+export * from '#modules/reviews/reviews.d.ts'
 export * from '#modules/auth/auth.d.ts'

36a06656f38ae940267b66c2691b21e86a8967b6
COLL SÁNCHEZ <raucolsan@alu.edu.gva.es>
2026-01-14 19:28:03 +0100

    Create user customer, miss validations

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -1,8 +1,37 @@
+import { parseDate } from '../commons/index.js'
+import { Customer, Employee, validateCustomer } from './users.model.js'
+
 export async function createUser(req, res) {
   try {
     const userData = req.body
+
+    const role = userData.role
+
     if (!userData) {
       return res.status(400).json({ message: 'Datos de usuarios no proporcionados' })
+    }
+
+    if (role === 'customer') {
+      try {
+        const validatedData = validateCustomer(userData)
+        const birthDate = parseDate(userData.birthDate)
+
+        const now = new Date()
+        now.setHours(0, 0, 0, 0)
+
+        console.log(now)
+        console.log(birthDate)
+
+        const newCustomer = new Customer({
+          ...validatedData,
+          birthDate,
+        })
+
+        const customerSaved = await newCustomer.save()
+        res.status(201).send(customerSaved)
+      } catch (err) {
+        return res.status(400).json(err)
+      }
     }
   } catch (error) {
     console.error(error)

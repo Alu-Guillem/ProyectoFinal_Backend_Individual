1531a7a4156fa942236cb2c72f632b55153b32f1
RaCs55 <racosan55@gmail.com>
2026-02-02 11:33:46 +0100

    feat: Añadidas nuevas rutas (me, employees y customers) que envian los datos segun el rol

--- src/modules/users/users.router.js
+++ src/modules/users/users.router.js
@@ -3,17 +3,26 @@ import {
   createCustomer,
   createEmployee,
   deleteUser,
+  getAllCustomers,
+  getAllEmployees,
+  getMe,
   getOneUser,
   getUsers,
   updateUser,
 } from '#modules/users/users.controller.js'
 const router = Router()
 
-router.post('/', createCustomer)
+router.post('/customer', createCustomer)
 
 router.post('/employee', createEmployee)
 
 router.get('/', getUsers)
+
+router.get('/me', getMe)
+
+router.get('/employees', getAllEmployees)
+
+router.get('/customers', getAllCustomers)
 
 router.get('/:id', getOneUser)
 
